0cdb9e3dbb4a19b03522de333819ac1ccf209818
nickguerra <nickg@maliciousgames.com>
2022-11-30 21:04:21 -0600

    custom types and interfaces added

--- lesson04/build/js/main.js
+++ lesson04/build/js/main.js
@@ -2,27 +2,27 @@
 // Literal types
 let myName;
 let userName;
-userName = 'Amy';
-// functions 
+userName = 'Nick';
+// Functions
 const add = (a, b) => {
     return a + b;
 };
 const logMsg = (message) => {
     console.log(message);
 };
-logMsg('Hello!');
+logMsg('Hello');
 logMsg(add(2, 3));
 let subtract = function (c, d) {
     return c - d;
 };
 // interface mathFunction {
-//     (a: number, b: number): number
+//     (a: number, b: number) : number
 // }
 let multiply = function (c, d) {
     return c * d;
 };
 logMsg(multiply(2, 2));
-// optional parameters 
+// optional parameters
 const addAll = (a, b, c) => {
     if (typeof c !== 'undefined') {
         return a + b + c;
@@ -36,12 +36,12 @@ const sumAll = (a = 10, b, c = 2) => {
 logMsg(addAll(2, 3, 2));
 logMsg(addAll(2, 3));
 logMsg(sumAll(2, 3));
-logMsg(sumAll(undefined, 3));
-// Rest Parameters 
+logMsg(sumAll(undefined, 2, 3));
+// Rest Parameters
 const total = (a, ...nums) => {
     return a + nums.reduce((prev, curr) => prev + curr);
 };
-logMsg(total(10, 2, 3));
+logMsg(total(1, 2, 3, 4));
 const createError = (errMsg) => {
     throw new Error(errMsg);
 };
@@ -53,16 +53,14 @@ const infinite = () => {
             break;
     }
 };
-// custom type guard 
+// custom type guard
 const isNumber = (value) => {
-    return typeof value === 'number'
-        ? true : false;
+    return typeof value === 'number' ? true : false;
 };
-// use of the never type 
 const numberOrString = (value) => {
     if (typeof value === 'string')
         return 'string';
     if (isNumber(value))
         return 'number';
-    return createError('This should never happen!');
+    return createError('This should never happen');
 };
--- lesson04/src/main.ts
+++ lesson04/src/main.ts
@@ -1,95 +1,93 @@
-// Type Aliases 
-type stringOrNumber = string | number
+type stringOrNumber = string | Number;
 
-type stringOrNumberArray = (string | number)[]
+type stringOrNumberArray = (string | number) [];
 
 type Guitarist = {
-    name?: string,
-    active: boolean,
+    name?: String,
+    active: Boolean,
     albums: stringOrNumberArray
 }
 
-type UserId = stringOrNumber
+type UserId = stringOrNumber;
 
 // Literal types
-let myName: 'Dave'
+let myName: 'Nick';
 
-let userName: 'Dave' | 'John' | 'Amy'
-userName = 'Amy'
+let userName: 'Nick' | 'Keller' | 'Car';
+userName = 'Nick';
 
-// functions 
+// Functions
 const add = (a: number, b: number): number => {
-    return a + b
+    return a + b;
 }
 
 const logMsg = (message: any): void => {
-    console.log(message)
+    console.log(message);
 }
 
-logMsg('Hello!')
-logMsg(add(2, 3))
+logMsg('Hello');
+logMsg(add(2,3));
 
 let subtract = function (c: number, d: number): number {
-    return c - d
+    return c - d;
 }
 
-type mathFunction = (a: number, b: number) => number
+type mathFunction = (a: number, b: number) => number;
 // interface mathFunction {
-//     (a: number, b: number): number
+//     (a: number, b: number) : number
 // }
 
 let multiply: mathFunction = function (c, d) {
-    return c * d
+    return c * d;
 }
 
-logMsg(multiply(2, 2))
+logMsg(multiply(2, 2));
 
-// optional parameters 
+// optional parameters
 const addAll = (a: number, b: number, c?: number): number => {
     if (typeof c !== 'undefined') {
-        return a + b + c
+        return a + b + c;
     }
-    return a + b
+    return a + b;
 }
 
 // default param value
 const sumAll = (a: number = 10, b: number, c: number = 2): number => {
-    return a + b + c
+    return a + b + c;
 }
 
-logMsg(addAll(2, 3, 2))
-logMsg(addAll(2, 3))
-logMsg(sumAll(2, 3))
-logMsg(sumAll(undefined, 3))
+logMsg(addAll(2,3,2));
+logMsg(addAll(2,3));
+logMsg(sumAll(2,3));
+logMsg(sumAll(undefined, 2,3));
 
-// Rest Parameters 
+// Rest Parameters
 const total = (a: number, ...nums: number[]): number => {
     return a + nums.reduce((prev, curr) => prev + curr)
 }
 
-logMsg(total(10, 2, 3))
+logMsg(total(1,2,3,4));
 
 const createError = (errMsg: string): never => {
-    throw new Error(errMsg)
+    throw new Error(errMsg);
 }
 
 const infinite = () => {
     let i: number = 1
-    while (true) {
-        i++
-        if (i > 100) break
+    while(true) {
+        i++;
+        if (i > 100) break;
     }
 }
 
-// custom type guard 
+// custom type guard
 const isNumber = (value: any): boolean => {
-    return typeof value === 'number'
-        ? true : false
+    return typeof value === 'number' ? true : false;
 }
 
-// use of the never type 
 const numberOrString = (value: number | string): string => {
-    if (typeof value === 'string') return 'string'
-    if (isNumber(value)) return 'number'
-    return createError('This should never happen!')
+    if (typeof value === 'string') return 'string';
+    if (isNumber(value)) return 'number';
+    return createError('This should never happen');
 }
+
