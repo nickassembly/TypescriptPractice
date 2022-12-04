4310d26351b914c8264a4eb072bdc52e545a35f7
nickguerra <nickg@maliciousgames.com>
2022-12-04 05:53:32 -0600

    Finished creating todo list

--- lesson11/src/model/FullList.ts
+++ lesson11/src/model/FullList.ts
@@ -11,42 +11,44 @@ interface List {
 
 export default class FullList implements List {
 
-    static instance: FullList = new FullList()
+    static instance: FullList = new FullList();
 
-    private constructor(private _list: ListItem[] = []) { }
+    private constructor(private _list: ListItem[] = []){}
 
     get list(): ListItem[] {
-        return this._list
+        return this._list;
     }
 
     load(): void {
-        const storedList: string | null = localStorage.getItem("myList")
-        if (typeof storedList !== "string") return
+        const storedList: string | null = localStorage.getItem("myList");
+        if (typeof storedList !== "string") return;
 
-        const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
+        const parsedList: {_id: string, _item: string, _checked: boolean}[] = JSON.parse(storedList);
 
         parsedList.forEach(itemObj => {
-            const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked)
-            FullList.instance.addItem(newListItem)
+            const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked);
+            FullList.instance.addItem(newListItem);
         })
     }
 
     save(): void {
-        localStorage.setItem("myList", JSON.stringify(this._list))
+        localStorage.setItem("myList", JSON.stringify(this._list));
     }
 
     clearList(): void {
-        this._list = []
-        this.save()
+        this._list = [];
+        this.save();
     }
 
     addItem(itemObj: ListItem): void {
-        this._list.push(itemObj)
-        this.save()
+        this._list.push(itemObj);
+        this.save();
     }
 
     removeItem(id: string): void {
-        this._list = this._list.filter(item => item.id !== id)
-        this.save()
+        this._list = this._list.filter(item => item.id !== id);
+        this.save();
     }
+
+
 }
